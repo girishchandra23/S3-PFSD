e3ddd34faee610b4be07819846c210cbceab0d8b
girish chandra <@girishchandra23>
2022-08-22 11:42:42 +0530

    Commit Done on 22-08-22 (11:41AM)

---
+++ prog1'.py
@@ -0,0 +1,10 @@
+ class Calc:
+
+
+    a=int(input(5))
+    Area=a*a
+    perimeter=a*a
+    print(" The area of the circle"+Area)
+    print(" The Area of the perimeter"+perimeter)
+    print("")
+
